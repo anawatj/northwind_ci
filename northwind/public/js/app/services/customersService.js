5c56f8bdfdb291e78fabc147404a02a85a5fc22a
anawatj <ajarusiripot@gmail.com>
2015-11-17 13:34:32 +0700

    add angular service

--- northwind/public/js/app/services/customersService.js
+++ northwind/public/js/app/services/customersService.js
@@ -1,1 +1,18 @@
-customersService.js
+app.factory('customersService',['$http',function($http)
+{
+	var factory = {};
+	factory.getAll=function()
+	{
+		return $http.get(url+"customers/all");
+	};
+	factory.getById=function(id)
+	{
+		return $http.get(url+"customers/single",{params:{key:id}});
+	};
+	factory.save=function(model)
+	{
+		return $http.post(url+"customers/save",model);
+	};
+
+	return factory;
+}]);
--- northwind/public/js/app/services/masterService.js
+++ northwind/public/js/app/services/masterService.js
@@ -1,1 +1,22 @@
-masterService.js
+app.factory('masterService',['$http',function($http)
+{
+		var factory = {};
+		factory.getAllCountry=function()
+		{
+			return $http.get(url+"master/getAllCountry");
+		};
+		factory.getAllCity=function(key)
+		{
+			return $http.get(url+"master/getAllCity",{params:{id:key}});
+
+		};
+		factory.getAllRegion=function()
+		{
+			return $http.get(url+"master/getAllRegion");
+		};
+		factory.getAllDemographic=function()
+		{
+			return $http.get(url+"master/getAllDemographic");
+		};
+		return factory;
+}]);
